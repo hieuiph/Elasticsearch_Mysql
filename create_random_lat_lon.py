347c65a7887777cc96300c888fb329fa7fa5078c
hieuiph <hieuiph@gmail.com>
2017-05-10 03:35:31 +0000

    Create new database with more columns

--- create_random_lat_lon.py
+++ create_random_lat_lon.py
@@ -5,10 +5,26 @@ conn = mysql.connect(host='127.0.0.1',port = 3306, user='root', passwd='', db='h
 
 with conn:
          cursor = conn.cursor()
-         for i in range(1,10001):
+      
+         query = """CREATE TABLE IF NOT EXISTS lat_lon_test 
+                                 (id int(11) PRIMARY KEY AUTO_INCREMENT,
+                                  title varchar(255), 
+                                  place varchar(255), 
+                                  lat float, 
+                                  lon float);"""
+         cursor.execute(query)
+         titles = ['don nha', 'sua chua do dien', 'IT', 'Boc vac', 'tiep thi', 'Phat to roi']
+         places = ['Dong Da', 'Hai Ba Trung', 'Truong Chinh', 'Bach Khoa', 'My Dinh']
+         
+         for i in range(1,1000001):
+             title = random.choice(titles)
+             place = random.choice(places)
              lat = ('%.6f' % (21.345654+random.random()/100))
              lon = ('%.6f' % (105.804817+random.random()/100))
-             sql = "INSERT INTO lat_lon(lat,lon) VALUES (%s,%s)"
-             cursor.execute(sql,(lat,lon))
-             
+             sql = "INSERT INTO lat_lon_test(title,place,lat,lon) VALUES (%s,%s,%s,%s)"
+             cursor.execute(sql,(title,place,lat,lon))
+   
+
+
+
 
